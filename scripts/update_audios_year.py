dbf5fbc4e41b4a5dc7299db25c25adc1714539e0
o-petrov <o.m.petrov@ya.ru>
2023-11-02 01:13:28 +0300

    Create a draft update_year script
    Create a script for updating audio's year from musicbrainz.org.
    Currently it's a draft. Should check, test, and maybe add other changes.

---
+++ scripts/update_audios_year.py
@@ -0,0 +1,55 @@
+from collections import defaultdict
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from time import sleep
+
+from src.database import database
+
+
+def main():
+    database.connect()
+
+    # выборка песен без обновлённого года выпуска
+    # мэтчим с MB по артистам и названию
+    audios = database.audios.find({"first_release_MB": {"$exists": False}}, {"link": 1, "title": 1, "artists": 1})
+
+    if not audios:
+        print("Все года проставлены")
+        return
+
+    minutes = len(audios) / 60
+    print(
+        f"Обновление годов для всех {len(audios)} треков с musicbrainz.org займёт около",
+        f"{minutes / 60:.1} часов" if minutes > 59 else f"{minutes:.1} минут"
+    )
+
+    for audio in audios:
+        query = urlencode({"query": audio["title"], "fmt": "json"})
+        req = Request(  # MB требуют осмысленный UA с контактами
+            'https://musicbrainz.org/ws/2/recording?' + query,
+            headers={'User-Agent': 'Year updating script (pg7av4dsh at mozmail dot com)'}
+        )
+        
+        with urlopen(req) as responce:
+            html = responce.read()
+        recs = json.loads(html)
+
+        # фильтруем найденные записи по точному совпадению с названием и артистами
+        # и берём дату/год выпуска
+        def artists(rec):
+            return set(credited["name"] for credited in rec["artist-credit"])
+        releases = [
+            r["first-release-date"] for r in recs["recordings"]
+            if r["title"] == audio["title"] and artists(rec) == set(audio["artists"])
+        ]
+
+        # так как даты мб вида '2023' и '2023-12-23' то просто сортируем алфавитно
+        first_release = int(sorted(releases)[0][:4])
+        # и записываем год в бд
+        database.audios.update_one({"link": audio["link"]}, {"$set": {"first_release_MB": first_release}}, upsert=True)
+        
+        sleep(1)  # вроде надо не чаще раза в секунду с одного IP
+
+
+if __name__ == '__main__':
+    main()
